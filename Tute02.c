627f58b75bb8ed716aeea36f885843bb677d77b5
IT21182914 <99383107+IT21182914@users.noreply.github.com>
2022-03-06 18:31:11 +0000

    Complete

--- Tute02.c
+++ Tute02.c
@@ -18,7 +18,37 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+
+  
+    int carKM;
+    int distance;
+    int outOfKM;
+
+
+    printf("Enter Your Distance: ");
+    scanf("%d",&carKM);
+
+
+
+   outOfKM = carKM - 30;
+
+    if (carKM<=30)
+
+    {
+        printf("\n\nFees for %dkm : Rs.%d",carKM,carKM * 50);
+    }
+
+    else if (30<carKM)
+    {
+        printf("Your %dkm fee is : Rs.%d " ,carKM,30*50 + outOfKM*40 );
+
+    }
+
+
+printf("\n\n");
+
   
   return 0;
 }
